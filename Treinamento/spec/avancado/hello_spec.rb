afc2197a98dd68c24f9c211b776fda433ce46de6
Rafael Ferraz <56234657+rafagerrard08@users.noreply.github.com>
2021-02-15 12:10:14 -0300

    Delete Treinamento/spec directory

--- Treinamento/spec/avancado/hello_spec.rb
+++
@@ -1,6 +0,0 @@
-describe "meu primeiro script" do
-    
-    it "visitar a página" do
-        puts "acessando a página"
-    end
-end
--- Treinamento/spec/basico/hashes_spec.rb
+++
@@ -1,8 +0,0 @@
-#hashes ter acesso ao valor com chaves
-
-carro = Hash[nome: 'civic',marca: 'honda',cor:'vermelho']
-puts carro[:marca]
-
-carro[:modelo] = 'SI'
-puts carro
-
