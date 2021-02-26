5271b98f9527afa771989ba829b8b7c0dfa5d086
Rafael Ferraz <56234657+rafagerrard08@users.noreply.github.com>
2021-02-26 18:41:15 -0300

    itens adicionados

---
+++ Treinamento/spec/radio_spec.rb
@@ -0,0 +1,20 @@
+describe 'botões de radio', :radio do
+
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/radios'
+        end
+
+        it 'selecao por ID' do
+            choose('cap')
+        end
+
+        it 'selecao por find e css selector' do
+            find('input[value=guardians]').click
+        end
+
+
+        after(:each) do
+            sleep 3
+
+        end
+end
