68183be0b719ddc0b8236a1f2bddf289d3ee1baf
Rafael Ferraz <56234657+rafagerrard08@users.noreply.github.com>
2021-03-05 14:42:58 -0300

    Update acesso_spec.rb

--- Treinamento/spec/acesso_spec.rb
+++ Treinamento/spec/acesso_spec.rb
@@ -1,4 +1,4 @@
-describe 'Meu primeiro script' do
+describe 'Meu primeiro script', :script do
     it 'Visitar a página' do
       visit 'https://www.dimepkairos.com.br/'
       expect(page.title).to eql "kairos"
