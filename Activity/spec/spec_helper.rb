408adfab258b3b088de5ded3c78045b159fedfb7
Rafael Ferraz <56234657+rafagerrard08@users.noreply.github.com>
2021-03-25 19:57:36 -0300

    Conjunto de testes de automação em linguagem Ruby com Capybara - Curso QA Ninja

--- Activity/spec/spec_helper.rb
+++ Activity/spec/spec_helper.rb
@@ -14,6 +14,9 @@ RSpec.configure do |config|
     config.shared_context_metadata_behavior = :apply_to_host_groups
 
     config.include Capybara::DSL
+    config.before(:example) do
+      page.current_window.resize_to(1920, 1080)
+    end
   end
 
   Capybara.register_driver :selenium_firefox do |app|
@@ -24,6 +27,3 @@ RSpec.configure do |config|
   config.default_driver = :selenium_chrome
   config.default_max_wait_time = 5
 end
-
-
-
